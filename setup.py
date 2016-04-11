b00e53eba028b05cb88a6b8b90e68425a7324656
Jari <jtorniainen@users.noreply.github.com>
2016-04-11 16:40:30 +0300

    adding setup-script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='event-logger',
+      version='0.0.1',
+      description='Command line event logger',
+      author=('Jari Torniainen, '
+              'Quantified Employee, '
+              'Finnish Institute of Occupational Health'),
+      author_email='jari.torniainen@ttl.fi',
+      url='https://github.com/jtorniainen/event-logger',
+      license='MIT',
+      packages=['event-logger'],
+      package_dir={'event-logger': 'event-logger'},
+      include_package_data=False,
+      entry_points={"console_scripts":
+                    ["event-logger = event-logger.event_logger:run_from_cli"]})
