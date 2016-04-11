7b57d8bccc4a685a66dd2c26d9b180e18868cabe
Jari <jtorniainen@users.noreply.github.com>
2016-04-11 16:56:13 +0300

    more tweaks to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup
 setup(name='event-logger',
       version='0.0.1',
       description='Command line event logger',
-      author=('Jari Torniainen, '
-              'Quantified Employee, '
-              'Finnish Institute of Occupational Health'),
+      author='Jari Torniainen, Quantified Employee, Finnish Institute of Occupational Health',
       author_email='jari.torniainen@ttl.fi',
       url='https://github.com/jtorniainen/event-logger',
       license='MIT',
