73ced27c663ebd3c26d20567d361ab2a5b128f2b
Jari <jtorniainen@users.noreply.github.com>
2016-04-12 08:14:15 +0300

    updated setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name='event_logger',
+setup(name='eventlog',
       version='0.0.1',
       description='Command line event logger',
       author='Jari Torniainen, Quantified Employee, Finnish Institute of Occupational Health',
       author_email='jari.torniainen@ttl.fi',
       url='https://github.com/jtorniainen/event-logger',
       license='MIT',
-      packages=['event_logger'],
-      package_dir={'event_logger': 'event_logger'},
+      packages=['eventlog'],
+      package_dir={'eventlog': 'eventlog'},
       include_package_data=False,
       entry_points={"console_scripts":
-                    ["event_logger = event_logger.event_logger:run_from_cli"]})
+                    ["eventlog = eventlog.eventlog:run_from_cli"]})
